da73b82a8c941c01973ce51169ab2d0702e2cae8
JoaoGoncalves <joaofgo@gmail.com>
2021-05-28 00:34:03 +0100

    Adds scripts for performing preparatory tasks

---
+++ RCODE/PrepareTrainDataPAs-v1.R
@@ -0,0 +1,99 @@
+
+library(raster)
+library(sf)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(exactextractr)
+library(stringi)
+library(stringr)
+library(rlang)
+library(crayon)
+library(randomForest)
+library(biomod2)
+library(biomod2plus)
+library(trend)
+library(diptest)
+
+## -------------------------------------------------------------------------------------- ##
+
+#setwd("C:/MyFiles/R-dev/Msc_MarianaOrsini_MEA")
+
+# Read ancillary functions
+source("./RCODE/_LibMainFunctions-v1.R")
+
+grid1k_wgs84 <- read_sf("./DATA_/VECTOR/Area_estudo/espinhaco_grid_WGS84.shp")
+
+dfPathList <- list.files("./DATA_/TABLES/_PRED_DATASETS/", ".rds$", full.names = TRUE)
+
+
+## -------------------------------------------------------------------------------------- ##
+
+spData <- readxl::read_excel("./DATA_/TABLES/SpeciesData/Amphibians/endemic_amphibia_Espinhaco_v2-1.xlsx")
+
+
+spDatasf <- prepSpData(spData, filterByMinObs = FALSE)
+
+spDataGrid <- prepSpDataWithGridID(spDatasf, grid1k_wgs84,
+                         filterByMinObs = FALSE, getCounts = TRUE)
+
+spCountsAmph <- spDataGrid[["spCounts"]] %>% st_drop_geometry()
+write_csv(spCountsAmph,"./OUT/Species1KmCounts_Amphibians-v1.csv")
+
+
+createTrainData (spData           = spData,
+                   spName         = NULL,
+                   sfGrid         = grid1k_wgs84,
+                   dfPathList     = dfPathList,
+                   spNameCol      = "Species",
+                   lonCol         = "Lon",
+                   latCol         = "Lat",
+                   yearCol        = "Year",
+                   yrStart        = 1985,
+                   yrEnd          = 2019,
+                   filterByMinObs = TRUE,
+                   nmin           = 30,
+                   removeDups     = TRUE,
+                   nPAsets        = 10,
+                   nPAperSet      = "equal",
+                   progressBar    = TRUE,
+                   outDir         = "./DATA_/TABLES/_TRAIN_DATASETS/")
+
+## -------------------------------------------------------------------------------------- ##
+
+spData <- readxl::read_excel("./DATA_/TABLES/SpeciesData/Birds/DH_Highland_Birds-v2.xlsx")
+
+
+spDatasf <- prepSpData(spData, 
+                       filterByMinObs = FALSE,
+                       spNameCol      = "SpeciesName",
+                       latCol         = "dLat",
+                       lonCol         = "dLon",
+                       yearCol        = "Year"
+                       )
+
+spDataGrid <- prepSpDataWithGridID(spDatasf, grid1k_wgs84,
+                                   filterByMinObs = FALSE, getCounts = TRUE)
+
+spCountsBirds <- spDataGrid[["spCounts"]] %>% st_drop_geometry()
+write_csv(spCountsBirds,"./OUT/Species1KmCounts_HighlandBirds-v1.csv")
+
+
+createTrainData (spData           = spData,
+                 spName         = NULL,
+                 sfGrid         = grid1k_wgs84,
+                 dfPathList     = dfPathList,
+                 spNameCol      = "SpeciesName",
+                 lonCol         = "dLon",
+                 latCol         = "dLat",
+                 yearCol        = "Year",
+                 yrStart        = 1985,
+                 yrEnd          = 2019,
+                 filterByMinObs = TRUE,
+                 nmin           = 30,
+                 removeDups     = TRUE,
+                 nPAsets        = 10,
+                 nPAperSet      = "equal",
+                 progressBar    = TRUE,
+                 outDir         = "./DATA_/TABLES/_TRAIN_DATASETS/BIRDS")
+
---
+++ RCODE/SelectPredictVars_RFitVarSel-v1.R
@@ -0,0 +1,89 @@
+
+
+library(raster)
+library(sf)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(exactextractr)
+library(stringi)
+library(stringr)
+library(rlang)
+library(crayon)
+library(randomForest)
+library(biomod2)
+library(biomod2plus)
+
+
+## -------------------------------------------------------------------------------------------- ##
+
+
+setwd("C:/MyFiles/R-dev/Msc_MarianaOrsini_MEA")
+
+source("./RCODE/_LibMainFunctions-v1.R")
+
+
+## -------------------------------------------------------------------------------------------- ##
+
+
+# Table with groups of variables: STATIC (like climate) and dynamic 
+# (annual land use and satellite indices)
+varGroups = read_csv("./DATA_/varNames-v1.csv")
+
+# Base directory holding the train data/matrix
+baseDir <- "./DATA_/TABLES/_TRAIN_DATASETS/BIRDS/"
+
+# The actual column range of predictor variables to use that is in the TRAIN DATASETS files
+varRange <- 8:52
+
+
+## ------------------------------------------------------------------------------------------- ##
+
+
+# List all train data and matrices files
+
+flTrain <- list.files(baseDir, pattern="_TrainData_", full.names = TRUE)
+flMat   <- list.files(baseDir, pattern="_TrainMatrix_", full.names = TRUE)
+
+flTrain <- flTrain[grepl(".rds$", flTrain)]
+flMat   <- flMat[grepl(".rds$", flMat)]
+
+
+# Run the variable selection algorith by species
+#
+#
+
+for(i in 1:length(flTrain)){
+  
+  trainData <- read_rds(flTrain[i])
+  trainData <- trainData %>% select(-WETL)
+  trainMatrix <- read_rds(flMat[i])
+  predVars    <- colnames(trainData)[varRange]
+  
+  spName <- trainData[1,"SpeciesName"]
+  
+  cat(green("\n\n*****************************************************\n"))
+  cat(green("*** Processing species name:", spName, "\n"))
+  cat(green("*****************************************************\n\n"))
+  
+  selVars <- multiRoundIterVarSel(trainData, 
+                                  trainMatrix, 
+                                  predVars, 
+                                  ntree        = 1000,
+                                  nmax         = "10-rule", 
+                                  tenRuleNmax  = 10,
+                                  thresh       = 0.75, 
+                                  aggFun       = median,
+                                  method       = "spearman",
+                                  varGroups, 
+                                  doNZV        = TRUE, 
+                                  doHighCor    = TRUE, 
+                                  highCorVal   = 0.95,
+                                  pbar         = TRUE, 
+                                  verbose      = TRUE)
+  
+  print(selVars)
+  outFn <- paste(baseDir, gsub("\\ +","_",spName),"_SelectedVars_v1.rds",sep="")
+  write_rds(selVars,file = outFn)
+}
+
